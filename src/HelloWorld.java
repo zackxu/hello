a6a0ef15f0fe722aa5a3b73b1b1888b2deee956e
Zack Xu <z-xu@live.com>
2013-03-29 16:24:11 -0500

    2. User 2 commit 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
 
 public class HelloWorld {
+	public static void test() {
+		System.out.println("test");
+	}
+
 	public static void main (String[] args) {
-		System.out.println("It's a small world after all.");
+		System.out.println("It's a small world after all. - Please use captital letters for titles.");
+		System.out.println("Line2");
 	}
 
 }
