199185234bd2fb63892c788092c4b7a4d7daac3d
Zack Xu <z-xu@live.com>
2013-04-01 08:50:59 -0500

    First push to Github

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("It's a small world after all. - Please use captital letters for titles.");
 		System.out.println("Line2");
+		System.out.println("Line3");
 	}
 
 }
